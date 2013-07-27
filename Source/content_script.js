30fd290519336b74be7a1819757351a4b74bf971
pablissima <paulettm@microsoft.com>
2013-07-26 18:43:28 -0700

    Update content_script.js

--- Source/content_script.js
+++ Source/content_script.js
@@ -31,10 +31,9 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bMy butt\b/g, "My shiny metal ass");
+	v = v.replace(/\bmy butt\b/g, "my shiny metal ass");
+;
 	
 	textNode.nodeValue = v;
 }
